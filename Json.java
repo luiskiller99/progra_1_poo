0048134b534d66ed3c52ab527bb4b269daf6ea5f
ErickElizondo <43624905+ErickElizondo@users.noreply.github.com>
2018-10-06 12:43:05 -0600

    Add files via upload
    Lee satisfactoriamente los archivos JSON.

--- Json.java
+++ Json.java
@@ -6,17 +6,57 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import java.nio.file.Files;
 
 public class Json{
-    static void leerJSON() throws FileNotFoundException, IOException, ParseException{
+    static void leerJSONusuarios() throws FileNotFoundException, IOException, ParseException{
         JSONParser parser=new JSONParser();
-        JSONArray arregloJson= (JSONArray) parser.parse(new FileReader("Ubicacion de archivo"));
+        JSONArray arregloJson= (JSONArray) parser.parse(new FileReader("D:/Descargas/GitHub/progra_1_poo/tarea_programada_1/usuarios/secretarias.json"));
+        
+        //System.out.print(arregloJson);
         
         for(int i=0;i<arregloJson.size();i++){
-            JSONObject elemento= (JSONObject) arregloJson.get(i);
-            System.out.println(elemento.get("tipo"));
-            System.out.println(elemento.get("nombre"));        
+            JSONObject elemento= (JSONObject) arregloJson.get(i);            
+            System.out.print(elemento.get("Nombre")+"/");            
+            System.out.print(elemento.get("Cantón")+"/");
+            System.out.print(elemento.get("Correo")+"/");
+            System.out.print(elemento.get("Señas")+"/");
+            System.out.print(elemento.get("Usuario")+"/");
+            System.out.print(elemento.get("Telefono")+"/");
+            System.out.print(elemento.get("Cedula")+"/");
+            System.out.print(elemento.get("Distrito")+"/");
+            System.out.print(elemento.get("Contraseña")+"/");
+            System.out.print(elemento.get("Provincia")+"\n");
+            
         }    
     }
+    
+    static void leerJSONpasajeros() throws FileNotFoundException, IOException, ParseException{
+        JSONParser parser=new JSONParser();
+        JSONArray arregloJson=(JSONArray) parser.parse(new FileReader("D:/Descargas/GitHub/progra_1_poo/tarea_programada_1/usuarios/file1.json"));
+        for(int i=0;i<arregloJson.size();i++){
+            JSONObject elemento= (JSONObject) arregloJson.get(i);            
+            System.out.print(elemento.get("Nombre")+"/");            
+            System.out.print(elemento.get("Cantón")+"/");
+            System.out.print(elemento.get("Correo")+"/");
+            System.out.print(elemento.get("Señas")+"/");            
+            System.out.print(elemento.get("Telefono")+"/");
+            System.out.print(elemento.get("Cedula")+"/");
+            System.out.print(elemento.get("Distrito")+"/");
+            System.out.print(elemento.get("Provincia")+"\n");
+            
+        }  
+        
+    }
+    
+    static void main(String Args[]) throws FileNotFoundException, IOException, ParseException{
+        Json prueba=new Json();
+        System.out.print("=========================Usuarios=========================\n");
+        Json.leerJSONusuarios();
+        System.out.print("=========================Pasajeros=========================\n");
+        Json.leerJSONpasajeros();
+    }
+    
+    
 }
 
