1d56c1c89fe9b13d44a06b2d149ab82be8ffc6fb
ErickElizondo <43624905+ErickElizondo@users.noreply.github.com>
2018-10-06 10:26:16 -0600

    Add files via upload
    Ahora permite encriptar la contraseña generada.

--- RandomString.java
+++ RandomString.java
@@ -2,6 +2,8 @@ import java.security.SecureRandom;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Random;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.Cipher;
 
 public class RandomString {
 
@@ -14,7 +16,7 @@ public class RandomString {
           sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
        
        String respaldo=sb.toString();
-       System.out.print("hola "+respaldo+"\n");
+       //System.out.print("hola "+respaldo+"\n");
        for(char c:respaldo.toCharArray()){
            if (c==33||c==34||c==35||c==36||
            c==37||c==38||c==47||c==63||
@@ -29,12 +31,32 @@ public class RandomString {
        return sb.toString();
     }
     
+    public static String encrypt(String strClearText,String strKey) throws Exception{
+	String strData="";
+	
+	try {
+		SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
+		Cipher cipher=Cipher.getInstance("Blowfish");
+		cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
+		byte[] encrypted=cipher.doFinal(strClearText.getBytes());
+		strData=new String(encrypted);
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+		throw new Exception(e);
+	}
+	return strData;
+}
+    
         
     
-    public static void main(String args[]){
+    public static void main(String args[])throws Exception{
+        RandomString prueba=new RandomString();        
         int num=new Random().nextInt(5)+8; 
-        String hola=randomString(num);
+        String hola=prueba.randomString(num);
+        String prueba2=prueba.encrypt(hola, "1");
         System.out.print(hola+"\n");
+        System.out.print(prueba2);
     }
 }
     
