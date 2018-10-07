18051d1b65eafcd764f511287928f945f4729ca0
ErickElizondo <43624905+ErickElizondo@users.noreply.github.com>
2018-10-07 14:54:00 -0600

    Add files via upload
    Solo falta meter en memoria los choferes.

--- tarea_programada_1/usuarios/Json.java
+++ tarea_programada_1/usuarios/Json.java
@@ -9,9 +9,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import java.nio.file.Files;
+
 import tarea_programada_1.vehiculo.persona;
 import tarea_programada_1.vehiculo.direccion;
-
+import tarea_programada_1.vehiculo.viaje;
+import tarea_programada_1.vehiculo.fecha;
+import tarea_programada_1.vehiculo.vehiculo;
+import tarea_programada_1.vehiculo.licencia;
+import tarea_programada_1.vehiculo.chofer;
 public class Json{    
     
     public void leerJSONusuarios() {
@@ -67,6 +72,59 @@ public class Json{
         catch (ParseException e){
         System.out.print(e);        
         }
-    }    
+    }
+    
+    public void leerJSONviajes() {
+        try{
+        JSONParser parser=new JSONParser();
+        JSONArray arregloJson=(JSONArray) parser.parse(new FileReader("D:/Descargas/GitHub/progra_1_poo/tarea_programada_1/usuarios/viajes.json"));
+        for(int i=0;i<arregloJson.size();i++){
+            JSONObject elemento= (JSONObject) arregloJson.get(i);
+            fecha inicio=new fecha(Integer.parseInt(elemento.get("DiaI").toString()),
+            Integer.parseInt(elemento.get("MesI").toString()), Integer.parseInt(elemento.get("AñoI").toString()));
+            fecha finalizacion=new fecha(Integer.parseInt(elemento.get("DiaF").toString()),
+            Integer.parseInt(elemento.get("MesF").toString()), Integer.parseInt(elemento.get("AñoF").toString()));
+            viaje viaje=new viaje(elemento.get("Salida").toString(),elemento.get("Destino").toString(),
+            inicio, finalizacion, elemento.get("Estado").toString(), Integer.parseInt(elemento.get("Kilometros").toString()));
+            
+            
+        }
+    }
+    catch (FileNotFoundException e){
+          System.out.print(e);
+        
+        }
+        catch (IOException e){
+          System.out.print(e);
+        }
+        catch (ParseException e){
+        System.out.print(e);        
+        }
+    }
+    
+    public void leerJSONvehiculos() {
+        try{
+        JSONParser parser=new JSONParser();
+        JSONArray arregloJson=(JSONArray) parser.parse(new FileReader("D:/Descargas/GitHub/progra_1_poo/tarea_programada_1/usuarios/vehiculos.json"));
+        for(int i=0;i<arregloJson.size();i++){
+            JSONObject elemento= (JSONObject) arregloJson.get(i);
+            vehiculo vehiculo=new vehiculo(Integer.parseInt(elemento.get("Placa").toString()),
+            Integer.parseInt(elemento.get("Año").toString()),elemento.get("Color").toString(),
+            elemento.get("Marca").toString(),Integer.parseInt(elemento.get("Capacidad").toString()),
+            Integer.parseInt(elemento.get("Kilómetros").toString()),Integer.parseInt(elemento.get("Número").
+            toString()),elemento.get("Sede").toString(),elemento.get("Estado").toString());            
+        }
+    }
+    catch (FileNotFoundException e){
+          System.out.print(e);
+        
+        }
+        catch (IOException e){
+          System.out.print(e);
+        }
+        catch (ParseException e){
+        System.out.print(e);        
+        }
+    }
 }
 
