d1504e1bfeeb5ac457501714ad6bf4236ded8815
ErickElizondo <43624905+ErickElizondo@users.noreply.github.com>
2018-10-06 18:59:03 -0600

    Add files via upload

--- tarea_programada_1/usuarios/Bot.java
+++ tarea_programada_1/usuarios/Bot.java
@@ -1,20 +1,35 @@
+package tarea_programada_1.usuarios;
+
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot{
+    private String estado;
+    
+    public void setestado(String pestado){
+        estado=pestado;
+    }
+    
+    public String getestado(){
+        return estado;
+    }
+    
     @Override
     public void onUpdateReceived(Update update){
         System.out.println(update.getMessage().getFrom().getFirstName()+": "+update.getMessage()
         .getText());
         
         SendMessage sendMessage=new SendMessage().setChatId(update.getMessage().getChatId());
-        String mensaje=update.getMessage().getText();
-        System.out.print(mensaje);
-        if (mensaje.equals("!validarviaje"))
+        //String mensaje=update.getMessage().getText();
+        //System.out.print(mensaje);
+        String estado=this.getestado();
+        
+        
+        if (estado.equals("Aprovado"))
         sendMessage.setText("El viaje ha sido aceptado, ¡que disfrute su estadía!");
-        else if (mensaje.equals("!cancelarviaje"))
+        else if (estado.equals("Cancelado"))
         sendMessage.setText("Lo sentimos, pero el viaje ha sido cancelado.");
         else
         sendMessage.setText("Comando no reconocido.");
